a0c01f138753d457e0fba01b501f202f6a46420d
Zayd Patel <zayd62@gmail.com>
2019-04-02 14:12:44 +0100

    added code for validating the file format

--- connectz.py
+++ connectz.py
@@ -16,6 +16,8 @@ class ConnectBoard:
         self.path = path
 
 
+# end of class "Connect Board"
+
 def load_text_file(path):
     """
     Loads file from path
@@ -34,6 +36,30 @@ def load_text_file(path):
         return False
 
 
+def validate_file_format(path):
+    """
+    This validates the file to make sure it is in the correct format according to the specification
+
+    A valid format looks like the following:
+
+    ``1 2 3``
+
+    Args:
+        path (str): path to the game file
+
+    Returns:
+        bool: true if file follows the format, false if it does not
+    """
+
+    file = open(path, 'r')
+    first_line = file.readline().split(" ")
+
+    if len(first_line) == 3:
+        return True
+    else:
+        return False
+
+
 def print_output_code(code):
     """
     This function is responsible for printing the output code
@@ -43,6 +69,7 @@ def print_output_code(code):
     """
     print(code)
     sys.exit(0)
+
 
 def main():
     """
@@ -59,9 +86,18 @@ def main():
     if len(sys.argv) != 2:
         print("Provide one input file")
         sys.exit(0)
+
     # now attempt to load file. function returns a boolean. if false, then file does not exist
     if not load_text_file(sys.argv[1]):
         print_output_code("9")
+
+    # now to validate the input file
+    if not validate_file_format(sys.argv[1]):
+        print_output_code("8")
+    else:
+        print("valid")
+
+
 if __name__ == '__main__':
     # file is being run directly not as an imported module
     main()
