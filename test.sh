a57acf79ca7aea54d617522a9447d19745e31d3b
Zayd Patel <zayd62@gmail.com>
2019-04-04 13:02:39 +0100

    moved yobota provided tests into their own folder and adjusted test.sh

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 echo testing no command line arguments, expected value: Provide one input file, obtained
 python connectz.py
 echo testing file error, expected value: 9, obtained
-python connectz.py file.txt
+python connectz.py yobotaTests/file.txt
 echo testing invalid file, expected value: 8, obtained
-python connectz.py invalid_file.txt
+python connectz.py yobotaTests/invalid_file.txt
 echo testing illegal game, expected value: 7, obtained
-python connectz.py illegal_game.txt
+python connectz.py yobotaTests/illegal_game.txt
 echo testing illegal column, expected value: 6, obtained
-python connectz.py illegal_column.txt
+python connectz.py yobotaTests/illegal_column.txt
 echo testing illegal row, expected value: 5, obtained
-python connectz.py illegal_row.txt
+python connectz.py yobotaTests/illegal_row.txt
 echo testing only dimension lines, expected 3, obtained
 python connectz.py only_dimensions.txt
